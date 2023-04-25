2339132f3eaaadff13fa9ce5750d64e63abf8c0e
vick <victordickson474@gmail.com>
2023-04-25 15:41:00 +0100

    .

---
+++ frontend/src/app/components/Empty/index.tsx
@@ -0,0 +1,45 @@
+<<<<<<< HEAD
+import * as React from 'react';
+import style from './style.local.css';
+
+export namespace Emtpy {
+  export interface Props {
+    title: string;
+  }
+}
+
+export const Empty = ({ title }: Emtpy.Props) => {
+  return (
+    <div className={style['no-orders']}>
+      <span className={style.icon} />
+      <h3 className={style.title}>{title}</h3>
+      {/* <span>If you need more information please contact us:</span>
+      <a href={`mailto:${process.env.WP_HELP_EMAIL}`} className={style.email}>
+        {process.env.WP_HELP_EMAIL}
+      </a> */}
+    </div>
+  );
+};
+=======
+import * as React from 'react';
+import style from './style.local.css';
+
+export namespace Emtpy {
+  export interface Props {
+    title: string;
+  }
+}
+
+export const Empty = ({ title }: Emtpy.Props) => {
+  return (
+    <div className={style['no-orders']}>
+      <span className={style.icon} />
+      <h3 className={style.title}>{title}</h3>
+      {/* <span>If you need more information please contact us:</span>
+      <a href={`mailto:${process.env.WP_HELP_EMAIL}`} className={style.email}>
+        {process.env.WP_HELP_EMAIL}
+      </a> */}
+    </div>
+  );
+};
+>>>>>>> c66a194ed828982ae51a56ee1c6a93206da232a9
