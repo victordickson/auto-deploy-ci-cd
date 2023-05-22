1134a2d69b7f9bd75c115c6fbf095057ad395415
vick <victordickson474@gmail.com>
2023-05-22 17:42:05 +0100

    fix

--- frontend/src/app/components/NavbarButton/index.tsx
+++ frontend/src/app/components/NavbarButton/index.tsx
@@ -1,36 +1,26 @@
 import * as React from 'react';
-import { Component } from 'react';
-
+import { Button } from '../Button';
 import style from './style.local.css';
-import { Button } from '../../Button';
 
-export namespace NoFilteredOrders {
+export namespace NavbarButton {
   export interface Props {
-    searchText: string;
-    handleResetOrders: (searchText: string) => void;
+    title?: string;
+    children: any;
+    style?: React.CSSProperties;
+    className?: string;
+    onClick?(): void;
   }
 }
 
-export class NoFilteredOrders extends Component<NoFilteredOrders.Props> {
-  render() {
-    return (
-      <div className={style['no-orders']}>
-        <span className={style.icon} />
-        <h3 className={style.title}>{`We couldn't find any orders related to "${
-          this.props.searchText
-        }"`}</h3>
-        <span>
-          Please try with a different order number,
-          <br />
-          recipient name or company.
-        </span>
-        <Button
-          onClick={() => this.props.handleResetOrders('')}
-          className={style.Rectangle}
-        >
-          Back to all orders
-        </Button>
-      </div>
-    );
-  }
-}
+export const NavbarButton = (props: NavbarButton.Props) => {
+  return (
+    <Button
+      onClick={props.onClick}
+      title={props.title}
+      className={`${style['navbar-button']} ${style[props.className || '']}`}
+      style={props.style}
+    >
+      {props.children}
+    </Button>
+  );
+};
